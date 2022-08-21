206448e9d69ca7ff58860e8f1869165812267555
Shouhardik Saha <60508616+shouhardik@users.noreply.github.com>
2022-08-21 15:11:14 +0530

    error

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express=require('express')
+const mongoose=require('mongoose')
 const articleRouter=require('./routes/article')
 const app=express()
 
+mongoose.connect('mongodb://localhost/blog',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+
 app.set('view engine','ejs')
 
-app.use('/articles',articleRouter)
+app.use(express.urlencoded({extended:false}))
+
+
 
 app.get('/',(req,res)=>{
     const articles=[{
@@ -20,5 +28,5 @@ app.get('/',(req,res)=>{
     res.render('articles/index',{articles:articles})
 })
 
-
+app.use('/articles',articleRouter)
 app.listen(5000)
