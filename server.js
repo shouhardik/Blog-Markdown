243088ac17d6312b17c2fe3621fdf909029430b6
Shouhardik Saha <60508616+shouhardik@users.noreply.github.com>
2022-08-23 20:13:56 +0530

    40

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const mongoose=require('mongoose')
-const articleRouter=require('./routes/article')
+const Article=require('./models/article')
+const articleRouter=require('./routes/articles')
 const app=express()
 
 mongoose.connect('mongodb://localhost/blog',{
@@ -14,17 +15,9 @@ app.use(express.urlencoded({extended:false}))
 
 
 
+
 app.get('/',(req,res)=>{
-    const articles=[{
-        title:'Test Article 1',
-        createdAt:new Date(),
-        description:'Test description'
-    },
-    {
-        title:'Test Article 2',
-        createdAt:new Date(),
-        description:'Test description'
-    }]
+  
     res.render('articles/index',{articles:articles})
 })
 
---
+++ try.js
@@ -0,0 +1,8 @@
+let a=5
+let b=4
+//console.log(a*b)
+const f=(a,b)=>{
+    return a*b
+}
+c=f(a,b)
+console.log(c)
