e71664510dab4b2181e14a8d8879f4ec73ca29d4
Shouhardik Saha <60508616+shouhardik@users.noreply.github.com>
2022-08-24 22:11:03 +0530

    final code

--- server.js
+++ server.js
@@ -2,22 +2,24 @@ const express=require('express')
 const mongoose=require('mongoose')
 const Article=require('./models/article')
 const articleRouter=require('./routes/articles')
+const methodOverride=require('method-override')
 const app=express()
 
 mongoose.connect('mongodb://localhost/blog',{
     useNewUrlParser:true,
-    useUnifiedTopology:true
+    useUnifiedTopology:true,
+  //  useCreateIndexes:true
 })
 
 app.set('view engine','ejs')
 
 app.use(express.urlencoded({extended:false}))
 
+app.use(methodOverride('_method'))
 
 
-
-app.get('/',(req,res)=>{
-  
+app.get('/',async(req,res)=>{
+    const articles=await Article.find().sort({createdAt:'desc'})
     res.render('articles/index',{articles:articles})
 })
 
